62219d2c458d5a3f0c9810201b0abf89cc632d4b
Maria Savitskaya <slinchenkova@gmail.com>
2018-02-19 14:09:04 +0300

    Change project structure

---
+++ src/test/java/FirstTask.java
@@ -0,0 +1,26 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTask {
+
+    private WebDriver driver;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firstTask() {
+        driver.get("https://yandex.ru/");
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
